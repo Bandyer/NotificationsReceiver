fdbbf36fc4f8a209e2cf5edc3b0d957218ba5c98
Marco Brescianini <marco.brescianini@bandyer.com>
2020-09-16 14:53:00 +0200

    Update ViewController updating all information on view will appear

--- NotificationsReceiver/ViewController.swift
+++ NotificationsReceiver/ViewController.swift
@@ -10,17 +10,47 @@ class ViewController: UIViewController {
     @IBOutlet private var pushTextView: UITextView!
     @IBOutlet private var pushAuthLabel: UILabel!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        VoIPService.shared.onTokenUpdate = { [weak self] token in
+            guard let self = self else { return }
+            self.updateVoipToken()
+        }
+
+        PushService.shared.onTokenUpdate = { [weak self] token in
+            guard let self = self else { return }
+            self.updatePushToken()
+        }
+
+        PushService.shared.onAuthorizationStatusUpdate = { [weak self] in
+            guard let self = self else { return }
+            self.updatePushAuthStatus()
+        }
+    }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        PushService.shared.authorizationStatus { (status) in
+        updatePushAuthStatus()
+        updatePushToken()
+        updateVoipToken()
+    }
+
+    private func updatePushAuthStatus() {
+        PushService.shared.getAuthorizationStatus { (status) in
             DispatchQueue.main.async { [weak self] in
                 guard let self = self else { return }
-                
                 self.updatePushAuthLabel(status: status)
             }
         }
+    }
+
+    private func updateVoipToken() {
         updateVoipTokenField(token: VoIPService.shared.token)
+    }
+
+    private func updatePushToken() {
         updatePushTokenField(token: PushService.shared.token)
     }
 
@@ -34,6 +64,23 @@ class ViewController: UIViewController {
 
     private func updatePushAuthLabel(status: UNAuthorizationStatus) {
         pushAuthLabel.text = String(describing: status)
+    }
+
+}
+
+extension UNAuthorizationStatus: CustomStringConvertible {
+
+    public var description: String {
+        switch self {
+            case .notDetermined:
+                return "not determined"
+            case .denied:
+                return "denied"
+            case .authorized:
+                return "authorized"
+            case .provisional:
+                return "provisional"
+        }
     }
 
 }
