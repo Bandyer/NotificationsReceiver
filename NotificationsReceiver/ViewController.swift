d03f7e5fb76ccc51ed3a8cc90f81fda7a22c2b28
Marco Brescianini <marco.brescianini@bandyer.com>
2020-09-16 10:56:42 +0200

    Initial Commit

---
+++ NotificationsReceiver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotificationsReceiver
+//
+//  Created by Marco Brescianini on 16/09/2020.
+//  Copyright © 2020 Bandyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
