b5de4a36476e944875b2fe5cedff78c64db3d341
Marco Brescianini <marco.brescianini@bandyer.com>
2020-09-16 11:47:00 +0200

    Update view controller registering device for VoIP notifications on view did load

--- NotificationsReceiver/ViewController.swift
+++ NotificationsReceiver/ViewController.swift
@@ -1,20 +1,64 @@
 //
-//  ViewController.swift
-//  NotificationsReceiver
-//
-//  Created by Marco Brescianini on 16/09/2020.
 //  Copyright © 2020 Bandyer. All rights reserved.
 //
 
 import UIKit
+import PushKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PKPushRegistryDelegate {
+
+    private let registry: PKPushRegistry = PKPushRegistry(queue: .main)
+
+    @IBOutlet private var voipLabel: UILabel!
+    @IBOutlet private var pushLabel: UILabel!
+    @IBOutlet private var voipTextView: UITextView!
+    @IBOutlet private var pushTextView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        registerToVoIPNotifications()
     }
 
+    private func registerToVoIPNotifications() {
+        registry.delegate = self
+        registry.desiredPushTypes = [.voIP]
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        updateVoipTokenField(token: registry.pushToken(for: .voIP))
+    }
+
+    private func updateVoipTokenField(token: Data?) {
+        voipTextView.text = token?.tokenToString ?? ""
+    }
+
+    // MARK: PKPushRegistryDelegate
+
+    func pushRegistry(_ registry: PKPushRegistry, didUpdate pushCredentials: PKPushCredentials, for type: PKPushType) {
+        print("VoIP token updated: \(pushCredentials.tokenToString)")
+    }
+
+    func pushRegistry(_ registry: PKPushRegistry, didReceiveIncomingPushWith payload: PKPushPayload, for type: PKPushType) {
+        print("VoIP notification received: \(payload.dictionaryPayload)")
+    }
 
 }
 
+extension PKPushCredentials {
+
+    var tokenToString: String {
+        token.tokenToString
+    }
+}
+
+extension Data {
+
+    var tokenToString: String {
+        map {
+            String(format: "%02.2hhx", $0)
+        }.joined()
+    }
+}
